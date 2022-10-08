16e47b8160a9a0f5c10c689c132b5f8f8fc258ad
KY <KY@frontend.com>
2022-10-08 16:45:46 +0300

    version 2.0 optimization

---
+++ pages/drinks.tsx
@@ -0,0 +1,13 @@
+import { ProductsProps } from "../components/product/types";
+import ProductsPage from "../components/productsPage/ProductsPage";
+import { sanityClient } from "../lib/sanity/client";
+import { merchQueryDrinks } from "../lib/sanity/merchQuery";
+export default function Drinks({ products }: ProductsProps) {
+  return <ProductsPage products={products} />;
+}
+export async function getServerSideProps() {
+  const products = await sanityClient.fetch(merchQueryDrinks);
+  return {
+    props: { products },
+  };
+}
